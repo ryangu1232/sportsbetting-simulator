1214300d599970dea3a753f01011efb436470df1
Ryan Gu <ryangu1232@gmail.com>
2023-10-18 22:55:36 -0700

    added formula for capturing percent and prob

--- DATASET.PY/low_fantasy_dataset.py
+++ DATASET.PY/low_fantasy_dataset.py
@@ -3,6 +3,10 @@ import numpy as np
 import pandas as np
 import statistics as stat
 from matplotlib import pyplot as plt
+import math
+
+prize_picks_line = input("Input the Prizepicks Line: ")
+espn_projection = input("How many fantasy points is ESPN projecting?: ")
 
 projected_low_scores = np.array([9.8,	9.16,	9.39,	11.58,	8.2,	8.11,])
 actual_low_scores = np.array([2.5,   15.00,	5.40,	10.00,	8.00,	5.00,])
@@ -19,7 +23,29 @@ low_standard_dev = stat.stdev(low_errors)
 coef_of_variation = low_standard_dev/low_errors
 
 
- 
-print("Standard Deviation of sample is % s "
-    % low_standard_dev)
-print(coef_of_variation)
+def compute_probability(prize_picks_line, espn_projection, mean_error, std_deviation):
+    # Calculate the required error for the actual points to exceed the prize picks line
+    required_error = prize_picks_line - espn_projection
+    
+    # Calculate the Z-value
+    z_value = (required_error - mean_error) / std_deviation
+    
+    # Calculate the probability using the Z-table (cumulative distribution function of the normal distribution)
+    probability = 0.5 * (1 + math.erf(-z_value / math.sqrt(2)))
+    
+    # Convert probability to percentage
+    probability_percentage = probability * 100
+    
+    return z_value, probability_percentage
+
+# Example usage:
+prize_picks_line = 22
+espn_projection = 20
+mean_error = 0
+std_deviation = 3
+
+z, prob = compute_probability(prize_picks_line, espn_projection, mean_error, std_deviation)
+print(f"Z-value: {z:.2f}")
+print(f"Probability of scoring more than {prize_picks_line} points: {prob:.2f}%")
+
+
