c790f88b9e871847a1580ddb42a2ebb85204e585
Ryan Gu <ryangu1232@gmail.com>
2023-10-19 22:12:41 -0700

    added more data and model analysis

--- DATASET.PY/high_fantasy_dataset.py
+++ DATASET.PY/high_fantasy_dataset.py
@@ -10,9 +10,9 @@ espn_projection = float(input("How many fantasy points is ESPN projecting?: "))
 
 
 #dataset for high scores
-projected_high_scores = np.array([16.17,	18.61,	18.43,	19.03,	15.26,	17.71,])
-actual_high_scores = np.array([24.88,	19.08,	17,	10.2,	18.7,	21.5,])
-high_prizepicks_line = np.array([17,	19.5,	15.5,	16.5,	14.5,	18.5,])
+projected_high_scores = np.array([16.17,	18.61,	18.43,	19.03,	15.26,	17.71,	14.21,	16.27,	16.58,	14.02,	14.22,	13.34,	12.02,	10.62,])
+actual_high_scores = np.array([24.88,	19.08,	17,	10.2,	18.7,	21.5,	17.44,	22.70,	29.30,	1.50,	12.70,	19.00,	13.20,	9.50,])
+high_prizepicks_line = np.array([17,	19.5,	15.5,	16.5,	14.5,	18.5,	15,	15,	15.5,	11.5,	12.5,	12.5,	12.0,	11.0,])
 
 
 #calculates the errors, average of errors, and standard deviation of low errors to determine variation of errors. 
@@ -35,3 +35,4 @@ def compute_probability(prize_picks_line, espn_projection, mean_error, std_devia
 
 prob = compute_probability(prize_picks_line, espn_projection, high_mean_error, high_standard_dev)
 print(f"The probability of it hitting is {prob}")
+
--- DATASET.PY/low_fantasy_dataset.py
+++ DATASET.PY/low_fantasy_dataset.py
@@ -10,9 +10,9 @@ espn_projection = float(input("How many fantasy points is ESPN projecting?: "))
 
 
 #dataset for low scores
-projected_low_scores = np.array([9.8,	9.16,	9.39,	11.58,	8.2,	8.11,])
-actual_low_scores = np.array([2.5,   15.00,	5.40,	10.00,	8.00,	5.00,])
-low_prizepicks_line = np.array([8.5,	8.5,	8,	9.5,	8,	7.5,])
+projected_low_scores = np.array([9.8,	9.16,	9.39,	11.58,	8.2,	8.11,	8.6,	6.97,	7.61,])
+actual_low_scores = np.array([2.5,	15.00,	5.40,	10.00,	8.00,	5.00,	7.20,	10,	7.00,])
+low_prizepicks_line = np.array([8.5,	8.5,	8,	9.5,	8,	7.5,	7.5,	7.5,	7.5,])
 
 
 #calculates the errors, average of errors, and standard deviation of low errors to determine variation of errors. 
